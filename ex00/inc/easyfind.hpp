3d277657c9067a6c295bdceacfa427bab40592f4
Max Maltas Sosa <mmaltas-@cbr2s5.42barcelona.com>
2025-02-09 12:24:42 +0100

    starting cpp08

---
+++ ex00/inc/easyfind.hpp
@@ -0,0 +1,15 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void easyfind(T arr, int b)
+{
+	//std::vector<int> arr;
+	auto iterFound = find(arr.begin(), arr.end(), b); 
+
+}
+
+#endif
